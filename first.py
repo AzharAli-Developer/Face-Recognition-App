72e9302bc102d81dbce3948f5c94f9852c06a913
AzharSindu <azharalisp059@gmail.com>
2024-06-01 18:23:48 +0500

    commit

---
+++ first.py
@@ -0,0 +1,23 @@
+#import opencv python
+import cv2
+
+
+face_cap=cv2.CascadeClassifier("C:/Users/AZHAR ALI/AppData/Local/Programs/Python/Python39/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")  #is used to detect your app
+#enable camera for live video
+video_cap=cv2.VideoCapture(0)
+while True :
+    ret, video_data=video_cap.read()
+    col=cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
+    faces=face_cap.detectMultiScale(
+        col,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(video_data,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow("vido_show",video_data)
+    if cv2.waitKey(10)==ord("c"):
+        break
+video_cap.release()
